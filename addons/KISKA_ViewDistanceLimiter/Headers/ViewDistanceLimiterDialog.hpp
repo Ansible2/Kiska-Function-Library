ecb6d988a2cbb6a29bea70009cc8dfdab001a658
Ansible2 <afoshark@yahoo.com>
2021-10-16 15:33:39 -0600

    Updated base config for VDL

--- addons/KISKA_ViewDistanceLimiter/Headers/ViewDistanceLimiterDialog.hpp
+++ addons/KISKA_ViewDistanceLimiter/Headers/ViewDistanceLimiterDialog.hpp
@@ -24,39 +24,39 @@ class KISKA_VDL_setting_ctrlGrp: RscControlsGroupNoScrollbars
 {
 	idc = -1;
 
-	x = POS_X(-7);
-	y = POS_Y(0);
-	w = POS_W(15);
+	x = POS_X(-7.75);
+	y = POS_Y(1);
+	w = POS_W(15.5);
 	h = POS_H(2.5);
+	class controls
+	{
+		class setButton : ctrlButton
+		{
+			idc = BUTTON_IDC;
+			text = "Button";
+			x = POS_W(0.75);
+			y = POS_H(0);
+			w = POS_W(10.75);
+			h = POS_H(1);
+		};
+		class settingEditBox: RscEdit
+		{
+			idc = EDIT_IDC;
+			text = "123";
+			x = POS_W(11.5);
+			y = POS_H(0);
+			w = POS_W(4);
+			h = POS_H(1);
+		};
+		class settingSlider: RscXSliderH
+		{
+			idc = SLIDER_IDC;
+			x = POS_W(0.75);
+			y = POS_H(1);
+			w = POS_W(14.75);
+			h = POS_H(1);
+		};
 
-	class setButton : ctrlButton
-	{
-		idc = BUTTON_IDC;
-		text = "Button";
-		x = POS_W(0);
-	    y = POS_H(0);
-	    w = POS_W(11.5);
-	    h = POS_H(1);
-		sizeEx = GUI_TEXT_SIZE(1);
-	};
-
-	class settingSlider: RscXSliderH
-	{
-		idc = SLIDER_IDC;
-		x = POS_W(0);
-	    y = POS_H(1);
-	    w = POS_W(15);
-	    h = POS_H(1.5);
-	};
-	class settingEditBox: RscEdit
-	{
-		idc = EDIT_IDC;
-		text = "123";
-		x = POS_W(11.5);
-	    y = POS_H(0);
-	    w = POS_W(3.5);
-	    h = POS_H(1);
-		sizeEx = GUI_TEXT_SIZE(1);
 	};
 };
 
@@ -64,9 +64,10 @@ class KISKA_VDL_setting_ctrlGrp: RscControlsGroupNoScrollbars
 class KISKA_viewDistanceLimiter_Dialog
 {
 	idd = VIEW_DISTANCE_LIMITER_DIALOG_IDD;
-	movingEnabled = true;
-	enableSimulation = true;
-	onLoad = "[_this select 0] call KISKA_fnc_VDL_dialogOnLoad";
+	movingEnabled = 1;
+	enableSimulation = 1;
+	//onLoad = "[_this select 0] call KISKA_fnc_VDL_dialogOnLoad";
+	onLoad = "[_this select 0] call OpenVDL";
 
 	class controlsBackground
 	{
@@ -78,6 +79,7 @@ class KISKA_viewDistanceLimiter_Dialog
         	y = POS_Y(-8);
         	w = POS_W(16);
         	h = POS_H(18.5);
+			colorBackground[] = GREY_COLOR(0,0.25);
 		};
 		class KISKA_VDL_mainHeaderText: Rsctext
 		{
@@ -90,7 +92,7 @@ class KISKA_viewDistanceLimiter_Dialog
         	h = POS_H(1);
 			colorBackground[] = PROFILE_BACKGROUND_COLOR(0.65);
 			style = ST_CENTER;
-			sizeEx = GUI_TEXT_SIZE(1);
+			//sizeEx = GUI_TEXT_SIZE(1);
 		};
 		class KISKA_VDL_systemOn_headerText: RscText
 		{
@@ -169,21 +171,25 @@ class KISKA_viewDistanceLimiter_Dialog
 			idc = 520007;
 			y = POS_Y(-7.5);
 
-			class setButton: setButton
+			class controls : controls
 			{
-				text = "Set Target FPS";
+				class setButton: setButton
+				{
+					text = "Set Target FPS";
+				};
+				class settingSlider: settingSlider
+				{
+					tooltip = "The FPS that you want to achieve to have while the system is running";
+					sliderPosition = 60;
+		            sliderRange[] = {15,144};
+		            sliderStep = 1;
+					lineSize = 1;
+				};
+				class settingEditBox: settingEditBox
+				{
+				};
 			};
-			class settingSlider: settingSlider
-			{
-				tooltip = "The FPS that you want to achieve to have while the system is running";
-				sliderPosition = 60;
-	            sliderRange[] = {15,144};
-	            sliderStep = 1;
-				lineSize = 1;
-			};
-			class settingEditBox: settingEditBox
-			{
-			};
+
 		};
 
 		/* -------------------------------------------------------------------------
@@ -195,20 +201,23 @@ class KISKA_viewDistanceLimiter_Dialog
 
 			y = POS_Y(-4.5);
 
-			class setButton: setButton
+			class controls : controls
 			{
-				text = "Set Min Object Distance";
-			};
-			class settingSlider: settingSlider
-			{
-				tooltip = "The minimum distance your Object View Distance can be set to";
-				sliderPosition = 500;
-	            sliderRange[] = {100,3000};
-	            sliderStep = 1;
-				lineSize = 10;
-			};
-			class settingEditBox: settingEditBox
-			{
+				class setButton: setButton
+				{
+					text = "Set Min Object Distance";
+				};
+				class settingSlider: settingSlider
+				{
+					tooltip = "The minimum distance your Object View Distance can be set to";
+					sliderPosition = 500;
+					sliderRange[] = {100,3000};
+					sliderStep = 1;
+					lineSize = 10;
+				};
+				class settingEditBox: settingEditBox
+				{
+				};
 			};
 		};
 
@@ -218,19 +227,21 @@ class KISKA_viewDistanceLimiter_Dialog
 		class KISKA_VDL_maxObjectDist_ctrlGrp: KISKA_VDL_minObjectDist_ctrlGrp
 		{
 			idc = 520010;
-
 			y = POS_Y(-1.5);
 
-			class setButton: setButton
+			class controls : controls
 			{
-				text = "Set Max Object Distance";
-			};
-			class settingSlider: settingSlider
-			{
-				tooltip = "The maximum distance your Object View Distance can be set to";
-			};
-			class settingEditBox: settingEditBox
-			{
+				class setButton: setButton
+				{
+					text = "Set Max Object Distance";
+				};
+				class settingSlider: settingSlider
+				{
+					tooltip = "The maximum distance your Object View Distance can be set to";
+				};
+				class settingEditBox: settingEditBox
+				{
+				};
 			};
 		};
 
@@ -241,18 +252,20 @@ class KISKA_viewDistanceLimiter_Dialog
 		{
 			idc = 520010;
 
-			y = POS_Y(0.5);
-
-			class setButton: setButton
+			y = POS_Y(1.5);
+			class controls : controls
 			{
-				text = "Set Terrain View Distance";
-			};
-			class settingSlider: settingSlider
-			{
-				tooltip = "The overall (static) view distance; this can be rather high without an issue.";
-			};
-			class settingEditBox: settingEditBox
-			{
+				class setButton: setButton
+				{
+					text = "Set Terrain View Distance";
+				};
+				class settingSlider: settingSlider
+				{
+					tooltip = "The overall (static) view distance; this can be rather high without an issue.";
+				};
+				class settingEditBox: settingEditBox
+				{
+				};
 			};
 		};
 
@@ -262,23 +275,24 @@ class KISKA_viewDistanceLimiter_Dialog
 		class KISKA_VDL_checkFreq_ctrlGrp: KISKA_VDL_setting_ctrlGrp
 		{
 			idc = 520008;
-
-			y = POS_Y(2.5);
-
-			class setButton: setButton
+			y = POS_Y(4.5);
+			class controls : controls
 			{
-				text = "Set Check Frequency";
-			};
-			class settingSlider: settingSlider
-			{
-				tooltip = "This is how often the view distance will be adjusted in seconds";
-				sliderPosition = 1;
-	            sliderRange[] = {0.01,10};
-	            sliderStep = 0.01;
-				lineSize = 0.05;
-			};
-			class settingEditBox: settingEditBox
-			{
+				class setButton: setButton
+				{
+					text = "Set Check Frequency";
+				};
+				class settingSlider: settingSlider
+				{
+					tooltip = "This is how often the view distance will be adjusted in seconds";
+					sliderPosition = 1;
+		            sliderRange[] = {0.01,10};
+		            sliderStep = 0.01;
+					lineSize = 0.05;
+				};
+				class settingEditBox: settingEditBox
+				{
+				};
 			};
 		};
 
@@ -288,23 +302,25 @@ class KISKA_viewDistanceLimiter_Dialog
 		class KISKA_VDL_incriment_ctrlGrp: KISKA_VDL_setting_ctrlGrp
 		{
 			idc = 520011;
+			y = POS_Y(7.5);
 
-			y = POS_Y(4.5);
-
-			class setButton: setButton
+			class controls : controls
 			{
-				text = "Set Increment Size";
-			};
-			class settingSlider: settingSlider
-			{
-				tooltip = "By how much will the view distance be adjusted in meters to achieve the target FPS each check frequency?"
-				sliderPosition = 25;
-	            sliderRange[] = {1,500};
-	            sliderStep = 5;
-				lineSize = 10;
-			};
-			class settingEditBox: settingEditBox
-			{
+				class setButton: setButton
+				{
+					text = "Set Increment Size";
+				};
+				class settingSlider: settingSlider
+				{
+					tooltip = "By how much will the view distance be adjusted in meters to achieve the target FPS each check frequency?"
+					sliderPosition = 25;
+		            sliderRange[] = {1,500};
+		            sliderStep = 5;
+					lineSize = 10;
+				};
+				class settingEditBox: settingEditBox
+				{
+				};
 			};
 		};
 
