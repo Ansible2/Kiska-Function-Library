91e8dd3a06dec7d26c08325b3269726ae11a1584
Ansible2 <afoshark@yahoo.com>
2022-09-12 20:50:53 -0600

    (#118): Added basic input/output controller

---
+++ addons/KISKA_SpeechRecognition/ArmaSpeech/SpeechRecognition/ArmaExtension.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using RGiesecke.DllExport;
+
+namespace SpeechRecognition
+{
+    public class ArmaExtension
+    {
+        public static ExtensionCallback callback;
+        public delegate int ExtensionCallback([MarshalAs(UnmanagedType.LPStr)] string name, [MarshalAs(UnmanagedType.LPStr)] string function, [MarshalAs(UnmanagedType.LPStr)] string data);
+
+#if WIN64
+		[DllExport("RVExtensionRegisterCallback", CallingConvention = CallingConvention.Winapi)]
+#else
+        [DllExport("_RVExtensionRegisterCallback@4", CallingConvention = CallingConvention.Winapi)]
+#endif
+        public static void RVExtensionRegisterCallback([MarshalAs(UnmanagedType.FunctionPtr)] ExtensionCallback func)
+        {
+            callback = func;
+        }
+
+
+        /// <summary>
+        /// Gets called when arma starts up and loads all extension.
+        /// It's perfect to load in static objects in a seperate thread so that the extension doesn't needs any seperate initalization
+        /// </summary>
+        /// <param name="output">The string builder object that contains the result of the function</param>
+        /// <param name="outputSize">The maximum size of bytes that can be returned</param>
+#if WIN64
+        [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
+#else
+        [DllExport("_RVExtensionVersion@8", CallingConvention = CallingConvention.Winapi)]
+#endif
+        public static void RvExtensionVersion(StringBuilder output, int outputSize)
+        {
+            InputOutputHandler.onGameStart(output,outputSize);
+        }
+
+
+        /// <summary>
+        /// The entry point for the default callExtension command.
+        /// </summary>
+        /// <param name="output">The string builder object that contains the result of the function</param>
+        /// <param name="outputSize">The maximum size of bytes that can be returned</param>
+        /// <param name="function">The string argument that is used along with callExtension</param>
+#if WIN64
+        [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+#else
+        [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+#endif
+        public static void RvExtension(StringBuilder output, int outputSize,
+            [MarshalAs(UnmanagedType.LPStr)] string function)
+        {
+            InputOutputHandler.onExtensionCalled(output, outputSize, function, callback);
+        }
+
+
+        /// <summary>
+        /// The entry point for the callExtensionArgs command.
+        /// </summary>
+        /// <param name="output">The string builder object that contains the result of the function</param>
+        /// <param name="outputSize">The maximum size of bytes that can be returned</param>
+        /// <param name="function">The string argument that is used along with callExtension</param>
+        /// <param name="args">The args passed to callExtension as a string array</param>
+        /// <param name="argsCount">The size of the string array args</param>
+        /// <returns>The result code</returns>
+#if WIN64
+        [DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
+#else
+        [DllExport("_RVExtensionArgs@20", CallingConvention = CallingConvention.Winapi)]
+#endif
+        public static int RvExtensionArgs(
+            StringBuilder output, 
+            int outputSize,
+            [MarshalAs(UnmanagedType.LPStr)] string function,
+            [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, 
+            int argCount
+        )
+        {
+            InputOutputHandler.onExtensionCalledWithArgs(output, outputSize, function, args, argCount, callback);
+            return 0;
+        }
+    }
+}
---
+++ addons/KISKA_SpeechRecognition/ArmaSpeech/SpeechRecognition/InputOutputHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static SpeechRecognition.ArmaExtension;
+
+namespace SpeechRecognition
+{
+    internal class InputOutputHandler
+    {
+        public static void onGameStart(
+            StringBuilder outputBuilder, 
+            int outputSizeLimit, 
+            ExtensionCallback callbackFunction = null
+        )
+        {
+            outputBuilder.Append("Test-Extension v1.0");
+        }
+
+        public static void onExtensionCalled(
+            StringBuilder outputBuilder,
+            int outputSizeLimit, 
+            string functionToRun, 
+            ExtensionCallback callbackFunction
+        )
+        {
+            callbackFunction.Invoke("SpeechArgCallback", "someFunction", "data");
+            outputBuilder.Append(functionToRun);
+        }
+        public static void onExtensionCalledWithArgs(
+            StringBuilder outputBuilder, 
+            int outputSizeLimit, 
+            string functionToRun, 
+            string[] args,
+            int argCount,
+            ExtensionCallback callbackFunction
+        )
+        {
+            callbackFunction.Invoke("SpeechArgCallback", "someFunction", "data");
+            foreach (var arg in args)
+            {
+                outputBuilder.Append(arg);
+            }
+        }
+    }
+}
