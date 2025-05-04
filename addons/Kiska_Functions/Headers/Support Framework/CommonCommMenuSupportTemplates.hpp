5d3189626e9053786e9ba5f074863ae9120c120f
Ansible2 <ansible2@proton.me>
2025-05-04 14:59:36 -0600

    #176 - added cas support examples

--- addons/Kiska_Functions/Headers/Support Framework/CommonCommMenuSupportTemplates.hpp
+++ addons/Kiska_Functions/Headers/Support Framework/CommonCommMenuSupportTemplates.hpp
@@ -113,12 +113,13 @@ class KISKA_abstract_commMenuCloseAirSupport
     {
         icon = CAS_ICON;
         onSupportSelected = "_this call KISKA_fnc_commMenu_openCas";
+        draw3dMarker = ON;
+        canSelectAttackDirection = ON;
     };
     
     class KISKA_supportDetails
     {
         numberOfUses = 1;
-        draw3dMarker = ON;
         onSupportAdded = "_this call KISKA_fnc_commMenu_onSupportAdded";
         onSupportRemoved = "_this call KISKA_fnc_commMenu_onSupportRemoved";
         onSupportCalled = "_this call KISKA_fnc_suppports_onCalledCloseAirSupport";
@@ -127,5 +128,244 @@ class KISKA_abstract_commMenuCloseAirSupport
     class KISKA_supportManagerDetails
     {
         picture = CAS_ICON;
+    };
+};
+
+class KISKA_support_commMenu_gunRun_template : KISKA_abstract_commMenuCloseAirSupport
+{
+    class KISKA_commMenuDetails : KISKA_commMenuDetails
+    {
+        text = "CAS - Gun Run";
+        aircraftClass = "B_Plane_CAS_01_dynamicLoadout_F";
+
+        class AttackTypes
+        {
+            class GunRun
+            {
+                label = "Gun Run";
+                allowDamage = OFF;
+                fireOrders[] = {
+                    {
+                        "Gatling_30mm_Plane_CAS_01_F",
+                        "",
+                        200,
+                        0.05,
+                        "",
+                        0.1
+                    }
+                };
+            };
+        };
+    };
+
+    class KISKA_supportManagerDetails : KISKA_supportManagerDetails
+    {
+        text = "CAS - Gun Run";
+    };
+};
+
+class KISKA_support_commMenu_gunsRockets_template : KISKA_abstract_commMenuCloseAirSupport
+{
+    class KISKA_commMenuDetails : KISKA_commMenuDetails
+    {
+        text = "CAS - Guns & Rockets";
+        aircraftClass = "B_Plane_CAS_01_dynamicLoadout_F";
+
+        class AttackTypes
+        {
+            class GunRun
+            {
+                label = "Gun Run";
+                allowDamage = OFF;
+                fireOrders[] = {
+                    {
+                        "Gatling_30mm_Plane_CAS_01_F",
+                        "",
+                        200,
+                        0.05,
+                        "",
+                        0.1
+                    }
+                };
+            };
+            class HERockets
+            {
+                label = "HE Rockets";
+                allowDamage = OFF;
+                fireOrders[] = {
+                    {
+                        "pylon",
+                        "PylonRack_7Rnd_Rocket_04_HE_F",
+                        7,
+                        0.5,
+                        "guide_to_strafe_target",
+                        0.01
+                    }
+                    {
+                        "pylon",
+                        "PylonRack_7Rnd_Rocket_04_HE_F",
+                        7,
+                        0.5,
+                        "guide_to_strafe_target",
+                        0.01
+                    }
+                };
+            };
+            class APRockets
+            {
+                label = "AP Rockets";
+                allowDamage = OFF;
+                fireOrders[] = {
+                    {
+                        "pylon",
+                        "PylonRack_7Rnd_Rocket_04_AP_F",
+                        7,
+                        0.5,
+                        "guide_to_strafe_target",
+                        0.01
+                    }
+                    {
+                        "pylon",
+                        "PylonRack_7Rnd_Rocket_04_AP_F",
+                        7,
+                        0.5,
+                        "guide_to_strafe_target",
+                        0.01
+                    }
+                };
+            };
+            class GunsAndHERockets
+            {
+                label = "Guns & HE Rockets";
+                allowDamage = OFF;
+                fireOrders[] = {
+                    {
+                        "Gatling_30mm_Plane_CAS_01_F",
+                        "",
+                        100,
+                        0.05,
+                        "",
+                        0.1
+                    }
+                    {
+                        "pylon",
+                        "PylonRack_7Rnd_Rocket_04_HE_F",
+                        7,
+                        0.5,
+                        "guide_to_strafe_target",
+                        0.01
+                    }
+                };
+            };
+            class GunsAndAPRockets
+            {
+                label = "Guns & AP Rockets";
+                allowDamage = OFF;
+                fireOrders[] = {
+                    {
+                        "Gatling_30mm_Plane_CAS_01_F",
+                        "",
+                        100,
+                        0.05,
+                        "",
+                        0.1
+                    }
+                    {
+                        "pylon",
+                        "PylonRack_7Rnd_Rocket_04_AP_F",
+                        7,
+                        0.5,
+                        "guide_to_strafe_target",
+                        0.01
+                    }
+                };
+            };
+        };
+    };
+
+    class KISKA_supportManagerDetails : KISKA_supportManagerDetails
+    {
+        text = "CAS - Guns & Rockets";
+    };
+};
+
+class KISKA_support_commMenu_bombs_template : KISKA_abstract_commMenuCloseAirSupport
+{
+    class KISKA_commMenuDetails : KISKA_commMenuDetails
+    {
+        text = "CAS - Bombs";
+        aircraftClass = "B_Plane_CAS_01_dynamicLoadout_F";
+
+        class AttackTypes
+        {
+            class LGB
+            {
+                label = "Laserguided Bomb";
+                allowDamage = OFF;
+                fireOrders[] = {
+                    {
+                        "pylon",
+                        "PylonMissile_1Rnd_Bomb_04_F",
+                        1,
+                        0.05,
+                        "",
+                        0.1
+                    }
+                };
+            };
+            class ClusterBomb
+            {
+                label = "Cluster Bomb";
+                allowDamage = OFF;
+                fireOrders[] = {
+                    {
+                        "pylon",
+                        "PylonMissile_1Rnd_BombCluster_01_F",
+                        1,
+                        0.05,
+                        "",
+                        0.1
+                    }
+                };
+            };
+        };
+    };
+
+    class KISKA_supportManagerDetails : KISKA_supportManagerDetails
+    {
+        text = "CAS - Bombs";
+    };
+};
+
+class KISKA_support_commMenu_napalm_template : KISKA_abstract_commMenuCloseAirSupport
+{
+    class KISKA_commMenuDetails : KISKA_commMenuDetails
+    {
+        text = "CAS - Napalm";
+        aircraftClass = "B_Plane_CAS_01_dynamicLoadout_F";
+
+        class AttackTypes
+        {
+            class Napalm
+            {
+                label = "4x Napalm Bombs";
+                allowDamage = OFF;
+                fireOrders[] = {
+                    {
+                        "pylon",
+                        "vn_bomb_f4_out_500_blu1b_fb_mag_x4",
+                        4,
+                        0.5,
+                        "guide_to_strafe_target",
+                        1
+                    }
+                };
+            };
+        };
+    };
+
+    class KISKA_supportManagerDetails : KISKA_supportManagerDetails
+    {
+        text = "CAS - Napalm";
     };
 };
